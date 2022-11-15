b102607f4e1476ee891b5e0641af2261b19fcb97
ArthurBrockelman <ArthurBrockelman@gmail.com>
2022-11-15 14:45:17 -0500

    Working on home page.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,25 @@
 import Header from "../../components/header/Header";
 import Slideshow from "../../components/slideshow/Slideshow";
-import GridCards from "../../components/gridcards/GridCards";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import UserMediaGridCards from "../../components/gridcards/UserMediaGridCards";
+import Sidebar from "../../components/sideBar/sideBar";
+import MediaSlideshow from "../../components/slideshow/MediaSlideShow"
 import { isAuthenticated } from "../../utils/authHelper";
 
 function Home(props) {
   return (
     <div className="Home">
+        
         <Header isAuthenticated={isAuthenticated()} />
-        <Slideshow/>
-        <GridCards/>
+        <Container>
+          <Row>
+            <Col xs={3}><Sidebar /></Col>
+            <Col xs={9}><MediaSlideshow/></Col>
+          </Row>
+          {isAuthenticated() ? <UserMediaGridCards /> : null}
+        </Container>
     </div>
   );
 }
